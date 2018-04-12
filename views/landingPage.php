07258b280f3930b3c9a39189af080bea1eaefd79
justindonthomas <justin.don.thomas@gmail.com>
2018-04-12 14:06:26 -0600

    Testing session stuff.

--- views/landingPage.php
+++ views/landingPage.php
@@ -14,7 +14,14 @@ session_start();
   <span class="leftalign"> Logged in as: <?php echo $_SESSION['s_user']; ?></span>
   <span class="rightalign"><a href="pageScripts/logout.php">Log Out.</a></span> <br>
   <p>Login successful <p>
-  Is admin: <?php echo $_SESSION['is_admin']; ?>
+      <!-- TODO build base page here.-->
+  Is admin: <?php if ($_SESSION['is_admin'] == 1) {
+      echo "Yes<br>";
+      } elseif ($_SESSION['is_admin'] == 0) {
+      echo "No.<br>";
+      } else {
+      echo "I don't know<br>";
+      } ?>
   </body>
 </html>
 
